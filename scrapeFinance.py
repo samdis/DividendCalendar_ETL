8ceda1e3249bfd6f2591bc7bc4d41ce811867103
Sam D <38329857+samdis@users.noreply.github.com>
2020-07-29 15:36:35 -0600

    Starting to integrate mongo - module issue

--- scrapeFinance.py
+++ scrapeFinance.py
@@ -1,25 +1,28 @@
-# https://www.thestreet.com/util/divs.jsp?date=09_10_2020
-
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
+# Data load job
 
 from datetime import timedelta, date
 
 import requests
 import time
 import json
+import pandas as pd
+import pandas_market_calendars
+
+
+print(pandas_market_calendars.get_calendar_names())
 
 from pymongo import MongoClient
-client = MongoClient() #get client
-db = client.development #get db
-collection = db.finance #get collection
+
+client = MongoClient()  # get client
+db = client.development  # get db
+collection = db.finance  # get collection
 
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-print('hello')
+
 start_date = date(2020, 7, 1)
 end_date = date(2020, 12, 31)
 for single_date in daterange(start_date, end_date):
@@ -28,9 +31,9 @@ for single_date in daterange(start_date, end_date):
     p = requests.get(url)
 
     cleanText = p.text
-
     if not cleanText:
         continue
+
     cleanText = cleanText.replace('results', '\"results\"')
     cleanText = cleanText.replace("symbol", '\"symbol\"')
     cleanText = cleanText.replace("name", '\"name\"')
@@ -39,8 +42,15 @@ for single_date in daterange(start_date, end_date):
     cleanText = cleanText.replace("exdate", '\"exdate\"')
 
     finance_data = json.loads(cleanText)
-
+    result = collection.insert_many(finance_data['results'])
     for results in finance_data['results']:
-        print(results['symbol'])
-        print(results['name'])
-        print(results['amount'])
+        record = {'symbol': finance_data['symbol'],
+                  'amount': finance_data['amount'],
+                  'exdate': finance_data['exdate'],
+                  'payment date': "01-01-2020"}
+        collection.insert_one(record)
+    #    print(results['symbol'])
+    #    print(results['name'])
+    #    print(results['amount'])
+
+    print(result.inserted_ids)
